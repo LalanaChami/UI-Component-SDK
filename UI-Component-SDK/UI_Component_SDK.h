15a2629ac8efa5a0ffc87302591bd1683a881665
Lalana <t.lalana@theglobalmarketaccess.com>
2023-11-29 11:30:09 +0530

    Initial Commit

---
+++ UI-Component-SDK/UI_Component_SDK.h
@@ -0,0 +1,18 @@
+//
+//  UI_Component_SDK.h
+//  UI-Component-SDK
+//
+//  Created by Amila Manathunga on 2023-11-29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UI_Component_SDK.
+FOUNDATION_EXPORT double UI_Component_SDKVersionNumber;
+
+//! Project version string for UI_Component_SDK.
+FOUNDATION_EXPORT const unsigned char UI_Component_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UI_Component_SDK/PublicHeader.h>
+
+
